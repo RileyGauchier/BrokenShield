7ddc07363b0501a5797044ad6d4131022688dd12
Riley <rileygauchier@gmail.com>
2021-01-09 06:25:45 -0800

    moved unity project into folder

---
+++ ProjectBrokenShield/Assets/Scripts/BasicUnit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BasicUnit : MonoBehaviour
+{
+    [SerializeField]
+    protected string unitName;
+
+    [SerializeField]
+    protected int moveAmount = 5;
+
+    [SerializeField]
+    protected int startingHealth = 10;
+
+    private int health;
+
+    private void Awake()
+    {
+        health = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
